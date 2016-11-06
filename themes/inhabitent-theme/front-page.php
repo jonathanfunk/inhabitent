f9c8ac24cb03cd061094c1c2fe45cc5b2e59af6f
Jonathan Funk <jonfunk88@hotmail.com>
2016-11-06 11:54:08 -0800

    styled hero section

---
+++ themes/inhabitent-theme/front-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+      <section class="hero">
+				<img src="<?php echo get_template_directory_uri() . '/images/inhabitent-logo-full.svg'; ?>" alt="Inhabitent Logo" />
+      </section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
--- themes/inhabitent-theme/header.php
+++ themes/inhabitent-theme/header.php
@@ -26,7 +26,9 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="container">
 					<div class="site-branding">
-						<img src="<?php echo get_template_directory_uri() . '/images/inhabitent-logo-tent.svg'; ?>" alt="Inhabitent Logo" />
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<img src="<?php echo get_template_directory_uri() . '/images/inhabitent-logo-tent.svg'; ?>" alt="Inhabitent Logo" />
+					  </a>
 						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					</div><!-- .site-branding -->
 
@@ -38,4 +40,4 @@
 				</div><!-- .container -->
 			</header><!-- #masthead -->
 
-			<div id="content" class="site-content container">
+			<div id="content" class="site-content">
