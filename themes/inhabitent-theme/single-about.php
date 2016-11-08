4f1bb744351afa92f7f39ca9a02f6d68a75cb2a1
Jonathan Funk <jonfunk88@hotmail.com>
2016-11-07 18:27:59 -0800

    product shop integrated

---
+++ themes/inhabitent-theme/single-about.php
@@ -0,0 +1,10 @@
+<?php /* Template Name: About Page */ ?>
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+    <section class="about-hero">
+
+    </section>
+  </main><!-- #main -->
+</div><!-- #primary -->
+<?php get_footer(); ?>
