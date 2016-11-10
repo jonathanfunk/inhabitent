1db54aa3a98b883a0d706545139f670b1ad7f198
Jonathan Funk <jonfunk88@hotmail.com>
2016-11-10 14:46:57 -0800

    jquery file implemented

--- themes/inhabitent-theme/single.php
+++ themes/inhabitent-theme/single.php
@@ -24,6 +24,7 @@ get_header(); ?>
 			?>
 
 		<?php endwhile; // End of the loop. ?>
+		<button type="button" id="close-comments">Close Comments</button>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
