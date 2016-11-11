f1e24bef7421c773db969c26993ad9772e7c9b58
Jonathan Funk <jonfunk88@hotmail.com>
2016-11-10 19:49:55 -0800

    about page styled

--- themes/inhabitent-theme/page-about.php
+++ themes/inhabitent-theme/page-about.php
@@ -3,7 +3,13 @@
 <div id="primary" class="content-area">
   <main id="main" class="site-main" role="main">
     <section class="about-hero">
-
+      <h1><?php the_title(); ?></h1>
+    </section>
+    <section class="about-wrap">
+      <h2>Our Story</h2>
+      <?php echo CFS()->get( 'our_story' ); ?>
+      <h2>Our Team</h2>
+      <?php echo CFS()->get( 'our_team' ); ?>
     </section>
   </main><!-- #main -->
 </div><!-- #primary -->
