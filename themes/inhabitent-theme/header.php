ee73c5f6cb980fe27a6e525a91b70417fdd2fda9
Jonathan Funk <jonfunk88@hotmail.com>
2016-11-12 20:21:32 -0800

    applied reverse nav to about page & single adventure page

--- themes/inhabitent-theme/header.php
+++ themes/inhabitent-theme/header.php
@@ -25,12 +25,9 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="container">
 					<div class="site-branding">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-							<!--<img src="<?php echo get_template_directory_uri() . '/images/inhabitent-logo-tent.svg'; ?>" alt="Inhabitent Logo" />-->
-					  </a>
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"></a>
 						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					</div><!-- .site-branding -->
-
 					<nav id="site-navigation" class="main-navigation" role="navigation">
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
--- themes/inhabitent-theme/js/scripts.js
+++ themes/inhabitent-theme/js/scripts.js
@@ -4,22 +4,25 @@
     $('input[type=search]').animate({width: 'toggle'}).focus();
   });
 
-  var bodyClasses = $("body").hasClass("home");
+  var homeClass = $("body").hasClass("home");
+  var adventureClass = $("body").hasClass("single-adventure");
+  var aboutClass = $("body").hasClass("page-template-page-about");
 
   //This will add class if body has certain class
-  if(bodyClasses){
+  if(homeClass || adventureClass || aboutClass){
     $("#masthead").addClass("reverse-nav");
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      var height = $(window).height();
+      if (scroll >= height & (homeClass || adventureClass || aboutClass)) {
+          $("#masthead").removeClass("reverse-nav");
+      } else {
+          $("#masthead").addClass("reverse-nav");
+      }
+    });
   }
 
 
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    var height = $(window).height();
-    if (scroll >= height & bodyClasses) {
-        $("#masthead").removeClass("reverse-nav");
-    } else {
-        $("#masthead").addClass("reverse-nav");
-    }
-  });
+
 
 })( jQuery );
