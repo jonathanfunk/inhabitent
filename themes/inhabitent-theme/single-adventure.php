a863f1794f9e58f5afdc98185aa8fb720f60f1af
Jonathan Funk <jonfunk88@hotmail.com>
2016-11-12 16:38:48 -0800

    single adventure page styled

--- themes/inhabitent-theme/single-adventure.php
+++ themes/inhabitent-theme/single-adventure.php
@@ -7,24 +7,24 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<article class="one-product container">
-				<?php while ( have_posts() ) : the_post(); ?>
-					<div class="adventure-img">
-						<?php the_post_thumbnail( 'category-thumb' ); ?>
-					</div>
-					<div class="adventure-desc">
-						<h1><?php the_title(); ?></h1>
-						<?php the_content(); ?>
-						<div class="social-wrap">
-							<p class="white-link"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i> Like</a></p>
-							<p class="white-link"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i> Tweet</a></p>
-							<p class="white-link"><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i> Pin</a></p>
-						</div>
-					</div>
-				<?php endwhile; // End of the loop. ?>
-			</article><!--.container-->
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div class="adventure-img">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_post_thumbnail( 'category-thumb' ); ?>
+			</div>
+
+			<div class="adventure-desc">
+				<h1><?php the_title(); ?></h1>
+				<h2>By <?php the_author(); ?></h2>
+					<?php the_content(); ?>
+				<div class="social-wrap">
+					<p class="white-link"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i> Like</a></p>
+					<p class="white-link"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i> Tweet</a></p>
+					<p class="white-link"><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i> Pin</a></p>
+				</div>
+			</div>
+		<?php endwhile; // End of the loop. ?>
+	</main><!-- #main -->
+</div><!-- #primary -->
 <?php get_footer(); ?>
